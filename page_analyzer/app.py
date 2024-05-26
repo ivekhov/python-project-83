d3df2aff7d76997d7dc928dc07c507a9e12f0a53
Ivan Vekhov <ivekhov@gmail.com>
2024-05-26 08:31:45 +0300

    add boostrap

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from flask import Flask
+from flask import render_template
 
 
 load_dotenv()
@@ -11,7 +12,15 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to Page Analyzer!'
+    return render_template('index.html')
+
+
+# @app.route('/urls')
+# def hello_world():
+    # return render_template('index.html')
+    # pass
+
+
 
 
 if __name__ == "__main__":
