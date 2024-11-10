d270192509a10b5d55c0f2d169578d1ed67d5194
Ivan Vekhov <ivekhov@gmail.com>
2024-11-10 23:47:25 +0300

    update readme and add connection to local postgres

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,11 +1,21 @@
+import logging
 import os
 
 from dotenv import load_dotenv
 from flask import Flask
 from flask import render_template
+import psycopg2
 
 
 load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+try:
+    conn = psycopg2.connect(DATABASE_URL)
+    logging.info("Connection to database established")
+except:
+    logging.error("Can`t establish connection to database")
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
@@ -13,14 +23,6 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-
-
-# @app.route('/urls')
-# def hello_world():
-    # return render_template('index.html')
-    # pass
-
-
 
 
 if __name__ == "__main__":
